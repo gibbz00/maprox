06e46e275fdad29f55ecdeeb77783f040b6ac337
gibbz00 <gabrielhansson00@gmail.com>
2023-05-20 16:20:44 +0200

    Initial `flatgeobuf` reading.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-use bevy::prelude::*;
+use flatgeobuf::{FallibleStreamingIterator, FgbReader};
+use std::{fs::File, io::BufReader};
+// use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    let mut file_buffer = BufReader::new(File::open("countries.fgb").unwrap());
+    let mut flatgeobuf_reader = FgbReader::open(&mut file_buffer)
+        .unwrap()
+        .select_all()
+        .unwrap();
+    while let Some(simple_feature) = flatgeobuf_reader.next().unwrap() {
+        println!("{:#?}", simple_feature.fbs_feature())
+    }
+
+    // App::new().add_plugins(DefaultPlugins).run();
 }
