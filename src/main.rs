293e80671c5f8cc0a4fea144a800b86bc444f23c
gibbz00 <gabrielhansson00@gmail.com>
2023-05-28 12:49:35 +0200

    Bump dependencies.

--- src/main.rs
+++ src/main.rs
@@ -63,7 +63,10 @@ fn read_flat_geobuf_example(
 fn camera(mut commands: Commands) {
     const ORBIT_CLAMP: f32 = (PI / 2.) * 0.95;
     commands.spawn((
-        Camera3dBundle::default(),
+        Camera3dBundle {
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 50.0)),
+            ..default()
+        },
         PanOrbitCamera {
             alpha_lower_limit: Some(-ORBIT_CLAMP),
             alpha_upper_limit: Some(ORBIT_CLAMP),
@@ -76,6 +79,9 @@ fn camera(mut commands: Commands) {
 
 fn clamp_zoom(mut orbit_cameras: Query<&mut PanOrbitCamera>) {
     for mut orbit_camera in orbit_cameras.iter_mut() {
-        orbit_camera.radius = f32::min(orbit_camera.radius, 200.0)
+        orbit_camera.radius = Some(f32::min(
+            orbit_camera.radius.expect("Initialized value"),
+            200.0,
+        ))
     }
 }
