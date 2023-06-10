3469916b770b01b1a690146a1d6b8a87d9b93a58
gibbz00 <gabrielhansson00@gmail.com>
2023-06-10 11:48:42 +0200

    exmaples - maprox-client: Do not use a task pool for the singular message loop task.

--- examples/maprox-client/src/main.rs
+++ examples/maprox-client/src/main.rs
@@ -1,7 +1,4 @@
-use bevy_tasks::{IoTaskPool, TaskPoolBuilder};
 use flatgeobuf::{FallibleStreamingIterator, FgbReader};
-use futures::{select, FutureExt};
-use futures_timer::Delay;
 use geozero::ToGeo;
 use log::info;
 use maprox_common::{Event, MaproxConnection, MAPROX_CONNECTION_URL};
@@ -20,21 +17,18 @@ async fn main() {
         .init();
 
     let (socket, loop_fut) = WebRtcSocket::new_reliable(MAPROX_CONNECTION_URL);
-    let mut maprox_connection = MaproxConnection::new(socket);
-    IoTaskPool::init(|| TaskPoolBuilder::default().build());
-    IoTaskPool::get().spawn(loop_fut).detach();
 
-    let timeout = Delay::new(Duration::from_millis(100));
-    futures::pin_mut!(timeout);
+    std::thread::spawn(move || {
+        let executor = async_executor::Executor::new();
+        let task = executor.spawn(loop_fut);
+        let _ = futures_lite::future::block_on(executor.run(task));
+    });
+
+    let mut maprox_connection = MaproxConnection::new(socket);
+    let mut sent_geometries = false;
 
     loop {
         maprox_connection.register_peers();
-
-        select! {
-            _ = (&mut timeout).fuse() => {
-                timeout.reset(Duration::from_millis(100));
-            }
-        }
 
         if maprox_connection.connected_peers_count() == 0 {
             info!("Waiting for a maprox connection.");
@@ -43,19 +37,22 @@ async fn main() {
 
         info!("Established a maprox connection!");
 
-        info!("Reading 'countries.fgb'");
-        let mut reader = BufReader::new(File::open("countries.fgb").unwrap());
-        let mut flatgeobuf_reader = FgbReader::open(&mut reader).unwrap().select_all().unwrap();
-        info!("Sending geometries.");
-        while let Some(simple_feature) = flatgeobuf_reader.next().unwrap() {
-            if let Ok(geometry) = simple_feature.to_geo() {
-                maprox_connection.send_event(Event::RenderGeometry(geometry));
+        if sent_geometries == false {
+            info!("Reading 'countries.fgb'");
+            let mut reader = BufReader::new(File::open("countries.fgb").unwrap());
+            let mut flatgeobuf_reader = FgbReader::open(&mut reader).unwrap().select_all().unwrap();
+            info!("Sending geometries.");
+            while let Some(simple_feature) = flatgeobuf_reader.next().unwrap() {
+                if let Ok(geometry) = simple_feature.to_geo() {
+                    maprox_connection.send_event(Event::RenderGeometry(geometry));
+                }
             }
+            info!("sent geometries");
+            sent_geometries = true;
         }
 
-        // WORKAROUND: maprox_connection is now async and can therefore not be awaited.
         std::thread::sleep(Duration::from_secs(1));
-        info!("sent geometries");
-        break;
+
+        maprox_connection.send_event(Event::Increment);
     }
 }
