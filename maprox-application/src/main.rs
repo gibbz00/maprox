31aef7be346ed56a1d0df45772796a0a9a6c4916
gibbz00 <gabrielhansson00@gmail.com>
2023-06-11 21:25:41 +0200

    Improve embedded window sizing.

--- maprox-application/src/main.rs
+++ maprox-application/src/main.rs
@@ -5,7 +5,15 @@ fn main() {
     let mut app = App::new();
 
     app.insert_resource(ClearColor(Color::WHITE))
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                fit_canvas_to_parent: true,
+                // Don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugin(ConnectionPlugin)
         .add_plugin(MapCameraPlugin)
         .run();
