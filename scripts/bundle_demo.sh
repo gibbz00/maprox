08482c047584103b7be96f870c443523b44c2a23
gibbz00 <gabrielhansson00@gmail.com>
2023-06-24 14:07:17 +0200

    Repo - Make use of examples and crates directories.

--- scripts/bundle_demo.sh
+++ scripts/bundle_demo.sh
@@ -5,8 +5,8 @@ MAPROX_APPLICATION="maprox-application"
 MAPROX_DEMO="maprox-demo"
 
 mkdir -p dist
-trunk build --release --public-url "./" --dist "dist" --filehash false "$MAPROX_DEMO/index.html"
-trunk build --release --public-url "./" --dist "dist/$MAPROX_APPLICATION" --filehash false "$MAPROX_APPLICATION/index.html"
+trunk build --release --public-url "./" --dist "dist" --filehash false "examples/$MAPROX_DEMO/index.html"
+trunk build --release --public-url "./" --dist "dist/$MAPROX_APPLICATION" --filehash false "crates/$MAPROX_APPLICATION/index.html"
 
 sed -i "s/\$MAPROX_APPLICATION_DIST/\.\/$MAPROX_APPLICATION\/index.html/g" "dist/index.html"
 
